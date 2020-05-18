ecd8c27ffc1f3aae5c5cccdf557cee5a8b53ce24
Jose Moreno Gomez <53275813+JoseMG-ux@users.noreply.github.com>
2020-05-18 01:02:42 -0600

    Submit a request and get a result

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
 //Creation of the Develop branch
 
-const Cotizador = new API('your api key')//In the https://www.cryptocompare.com/cryptopian/api-keys
+const Cotizador = new API("your api key"); //In the https://www.cryptocompare.com/cryptopian/api-keys
 
-const ui = new Interfaz();//Interface instance (ui.js)
+const ui = new Interfaz(); //Interface instance (ui.js)
 
 //Read the form
-const formulario = document.querySelector('#formulario');
-
+const formulario = document.querySelector("#formulario");
 
 //Eventlistener
-formulario.addEventListener('submit', (e) => {
-     e.preventDefault();
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-     //Read the selected currency
-     const monedaSelect = document.querySelector('#moneda');
-     const monedaSeleccionada = monedaSelect.options[monedaSelect.selectedIndex].value;
+  //Read the selected currency
+  const monedaSelect = document.querySelector("#moneda");
+  const monedaSeleccionada =
+    monedaSelect.options[monedaSelect.selectedIndex].value;
 
-     //Read the selected cryptocurrency
-     const cryptomonedaSelect = document.querySelector('#moneda');
-     const cryptomonedaSeleccionada = cryptomonedaSelect.options[cryptomonedaSelect.selectedIndex].value;
-     
-     //check that both fields have something selected
-     if(monedaSeleccionada === '' || cryptomonedaSeleccionada === ''){
-          //Launch an error alert (ui.js)
-          ui.mostrarMensaje('Both fields are mandatory.', 'alert bg-danger text-center');
-     } else {
-          //All right, check the API
-     }
-     console.log(monedaSeleccionada);
-     console.log(cryptomonedaSeleccionada);
-     
-})
+  //Read the selected cryptocurrency
+  const cryptomonedaSelect = document.querySelector("#moneda");
+  const cryptomonedaSeleccionada =
+    cryptomonedaSelect.options[cryptomonedaSelect.selectedIndex].value;
+
+  //check that both fields have something selected
+  if (monedaSeleccionada === "" || cryptomonedaSeleccionada === "") {
+    //Launch an error alert (ui.js)
+    ui.mostrarMensaje(
+      "Both fields are mandatory.",
+      "alert bg-danger text-center"
+    );
+  } else {
+    //All right, check the API
+    Cotizador.obtenerValores(monedaSeleccionada, cryptomonedaSeleccionada).then(
+      (data) => {
+        console.log(data);
+      }
+    );
+  }
+});
