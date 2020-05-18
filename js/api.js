b9044e978702ee6cb6f3aa66f6c48609cfd801a4
Jose Moreno Gomez <53275813+JoseMG-ux@users.noreply.github.com>
2020-05-18 01:02:52 -0600

    Submit a request and get a result

--- js/api.js
+++ js/api.js
@@ -1,22 +1,32 @@
-class API{
+class API {
+  constructor(apikey) {
+    this.apikey = apikey;
+  }
+  //get all the coins
+  async obtenerMonedasAPI() {
+    const url = `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${this.apikey}`;
 
-     constructor(apikey) {
-          this.apikey = apikey;
+    //Fetch to the API
+    const urlObtenerMonedas = await fetch(url);
 
-     }
-     //get all the coins
-     async obtenerMonedasAPI(){
-          const url = `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${this.apikey}`;
-          
-          //Fetch to the API
-          const urlObtenerMonedas = await fetch(url);
+    //Answer in JSON
+    const monedas = await urlObtenerMonedas.json();
 
-          //Answer in JSON
-          const monedas = await urlObtenerMonedas.json();
+    return {
+      monedas, //Coins
+    };
+  }
 
+  async obtenerValores(moneda, criptomoneda) {
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}&api_key=${this.apikey}`;
 
-          return{
-               monedas //Coins
-          }
-     }
+    //Consultar en rest API
+
+    const urlConvertir = await fetch(url);
+    const resultado = await urlConvertir.json();
+
+    return {
+      resultado,
+    };
+  }
 }
